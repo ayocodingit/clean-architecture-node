4d4744ea94a1eea9b62d5539c39741b84fdfb225
ayocodingit <ayocodingit@gmail.com>
2023-04-03 11:52:03 +0700

    add parameter http in constuctor handler example

--- src/internal/example/delivery/http/handler.ts
+++ src/internal/example/delivery/http/handler.ts
@@ -1,7 +1,8 @@
 import winston from 'winston'
+import Http from '../../../../transport/http/http'
 
 class Handler {
-    constructor(private logger: winston.Logger) {}
+    constructor(private logger: winston.Logger, private http: Http) {}
 }
 
 export default Handler
