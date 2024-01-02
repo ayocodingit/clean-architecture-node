4733718044b7d811454f72076fd282b2f44ace72
ayocodingit <ayocodingit@gmail.com>
2024-01-02 11:36:08 +0700

    refactor migration

--- src/database/sequelize/migrations/20231228040131-create-posts-table.ts
+++ src/database/sequelize/migrations/20231228040131-create-posts-table.ts
@@ -1,38 +1,38 @@
 import { QueryInterface, DataTypes, Sequelize } from 'sequelize'
 
-module.exports = {
-    async up(queryInterface: QueryInterface) {
-        await queryInterface.createTable('posts', {
-            id: {
-                allowNull: false,
-                primaryKey: true,
-                type: DataTypes.UUID,
-                defaultValue: DataTypes.UUIDV4,
-            },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            description: {
-                type: DataTypes.STRING,
-                allowNull: true,
-            },
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP()'),
-            },
-            updated_at: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP()'),
-            },
-        })
+const tableName = 'posts'
 
-        return queryInterface.addIndex('posts', ['title'])
-    },
+export async function up(queryInterface: QueryInterface) {
+    await queryInterface.createTable(tableName, {
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP()'),
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP()'),
+        },
+    })
 
-    async down(queryInterface: QueryInterface) {
-        return queryInterface.dropTable('posts')
-    },
+    return queryInterface.addIndex(tableName, ['title'])
+}
+
+export async function down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable(tableName)
 }
