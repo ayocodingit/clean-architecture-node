7e619b861f7536d2df55bd91d1d201ca115151dc
ayocodingit <ayocodingit@gmail.com>
2025-12-04 10:44:00 +0700

    chore: remove commented-out MongoDB connection code.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import config from './config/config'
-// import Mongo from './database/mongo/mongo'
 import Sequelize from './database/sequelize/sequelize'
 import Post from './modules/post/post'
 import Logger from './pkg/logger'
@@ -7,7 +6,6 @@ import Http from './transport/http/http'
 
 const Run = async () => {
     const logger = new Logger(config)
-    // const connection = await Mongo.Connect(logger, config)
     const connection = await Sequelize.Connect(config, logger)
 
     const http = new Http(logger, config, connection)
